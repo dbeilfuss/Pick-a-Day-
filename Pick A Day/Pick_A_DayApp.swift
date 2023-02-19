a79fe0a0a97d4fb505be2b5f885f118c96ce6e84
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-02-19 11:29:26 -0700

    Initial Commit

---
+++ Pick A Day/Pick_A_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_DayApp.swift
+//  Pick A Day
+//
+//  Created by Daniel Beilfuss on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
